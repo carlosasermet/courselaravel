86bebba31ff54e96159a5c051ae57c1e729998bc
carlosasermet <carlos@asermet.com.mx>
2016-11-26 10:47:34 -0600

    Travis CI

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'SomeRandomString', 'vjR39mSTXiHln9N86lrYWiCIkUmMFNXb'),
 
     'cipher' => 'AES-256-CBC',
 
