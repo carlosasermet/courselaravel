26fb01e9806a5d202ee8a1a49f6328c51a5971b8
carlosasermet <carlos@asermet.com.mx>
2016-11-26 11:26:37 -0600

    Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 Carlos Martínez Beltrán</div>
+                <div class="title">Laravel 5 Carlos Martínez Beltrán 23 años</div>
             </div>
         </div>
     </body>
