1b4f05ab37e8a7d963f57319c1ff928c6ae25e9a
carlosasermet <carlos@asermet.com.mx>
2016-11-26 11:20:15 -0600

    Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel 5 Carlos Martínez</div>
             </div>
         </div>
     </body>
